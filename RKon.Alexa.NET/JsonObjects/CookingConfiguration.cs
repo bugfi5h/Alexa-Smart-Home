f90892cf833f167ef0b82e8a17295f74f4b35b2d
RK <rene.kling@agentilo.com>
2018-01-30 07:41:58 +0100

    Added Cooking MOdes and Scopes

---
+++ RKon.Alexa.NET/JsonObjects/CookingConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using RKon.Alexa.NET.Types;
+using System.Collections.Generic;
+
+namespace RKon.Alexa.NET.JsonObjects
+{
+    /// <summary>
+    /// Configuration Payload In addition to the standard discovery format, the Cooking discovery response should include a configuration object
+    /// </summary>
+    public class CookingConfiguration
+    {
+        /// <summary>
+        /// Specifies the cooking modes supported for this appliance’s implementation of the Cooking interface. Cannot be empty or only contain OFF.
+        /// </summary>
+        [JsonProperty("supportedCookingModes")]
+        [JsonRequired]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public List<CookingModes> SupportedCookingModes { get; set; }
+
+        /// <summary>
+        /// True to indicates the appliance starts with an Alexa voice command, 
+        /// otherwise false indicates the appliance is set to the specified cooking mode,
+        /// but does not start until the customer presses the start button on the appliance. 
+        /// </summary>
+        [JsonProperty("supportsRemoteStart", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? SupportsRemoteStart { get; set; }
+
+        /// <summary>
+        /// Basic Constructor
+        /// </summary>
+        public CookingConfiguration()
+        {
+            SupportedCookingModes = new List<CookingModes>();
+        }
+    }
+}
---
+++ RKon.Alexa.NET/Types/CookingModes.cs
@@ -0,0 +1,29 @@
+﻿namespace RKon.Alexa.NET.Types
+{
+    /// <summary>
+    /// Represents the a cooking mode for a cooking appliance like a microwave oven. 
+    /// </summary>
+    public enum CookingModes
+    {
+        /// <summary>
+        /// Indicates a cooking appliance’s automated defrost mode.
+        /// </summary>
+        DEFROST,
+        /// <summary>
+        ///  	Indicates a cooking appliance is off.
+        /// </summary>
+        OFF,
+        /// <summary>
+        /// Indicates a cooking appliance’s preset mode or other automated cooking.
+        /// </summary>
+        PRESET,
+        /// <summary>
+        /// Indicates a cooking appliance’s automated reheating mode.
+        /// </summary>
+        REHEAT,
+        /// <summary>
+        /// Indicates a time and power level cooking setting.
+        /// </summary>
+        TIMECOOK
+    }
+}
